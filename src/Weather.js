22678dc0bbeafb7f06321878a2468402b286e501
FrauY <yuliayulina55omsk@gmail.com>
2023-08-05 00:05:08 +0200

    Display the weather added axios integration

---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiKey = `9eddb9ab087ae2f5e27fac758d4297e4`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <h2>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    </h2>
+  );
+}
