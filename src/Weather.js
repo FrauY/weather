d85e658cd003e9e24d1b18e803d12296a0025d66
FrauY <yuliayulina55omsk@gmail.com>
2023-08-05 00:07:39 +0200

    Changed loader color

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
         height="80"
         width="80"
         radius="9"
-        color="green"
+        color="blueviolet"
         ariaLabel="loading"
         wrapperStyle
         wrapperClass
